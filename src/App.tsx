282328b67364270307678686a47722c2bd6b424d
Francisco-93 <francisco.fro.agui@gmail.com>
2021-01-07 14:00:56 -0300

    Add Home Page

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,12 @@
 import './App.css';
-import Hello from './Hello'
 import Navbar from './Navbar';
+import Home from './Home';
 
 function App() {
   return (
     <div>
       <Navbar/>
+      <Home/>
     </div>
   );
 }

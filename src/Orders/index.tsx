12a5232d97892aaa5139eedbf6b6060f190a00da
Francisco-93 <francisco.fro.agui@gmail.com>
2021-01-08 14:19:49 -0300

    Map and Address

--- src/Orders/index.tsx
+++ src/Orders/index.tsx
@@ -1,10 +1,29 @@
+import { useEffect, useState } from 'react';
+import { fetchProducts } from '../api';
+import OrderLocation from './OrderLocation';
+import ProductsList from './ProductsList';
 import StepsHeader from './StepsHeader';
 import './style.css';
+import { Product } from './types';
 
 function Orders(){
+
+  const[products, setProducts] = useState<Product[]>([]);
+
+  console.log(products);
+
+  useEffect(()=>{
+      fetchProducts()
+      .then(response => setProducts(response.data))
+      .catch(error => console.log(error))
+
+  },[]);
+
   return(
     <div className="orders-container">
       <StepsHeader/>
+      <ProductsList products={products}/>
+      <OrderLocation/>
     </div>
   )
 } 
